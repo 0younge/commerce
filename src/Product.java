9d03a3e61296cd207acacee4f58b50b63f03ec6f
0younge <s45628439@gmail.com>
2026-03-26 16:21:25 +0900

    finish: STEP 1

---
+++ src/Product.java
@@ -0,0 +1,19 @@
+public class Product {
+
+    // 속성
+    String productName;
+    int price;
+    String description;
+    int quantity;
+
+    // 생성자
+    public Product(String productName, int price, String description, int quantity) {
+        this.productName = productName;
+        this.price = price;
+        this.description = description;
+        this.quantity = quantity;
+    }
+
+    // 기능
+
+}
